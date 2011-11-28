bb6f05e807a46868892786747071b95b2f771600
Hanfei Sun <hfsun.tju@gmail.com>
2011-11-28 07:03:08 -0500

    Add chrM

--- MPIPE/bed2fasta.py
+++ MPIPE/bed2fasta.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Time-stamp: <2011-11-28 06:18:11 hanfei>
+# Time-stamp: <2011-11-28 07:02:40 hanfei>
 """Module Description: Main executable for converting summits bed to fasta files
 
 Copyright (c) 2011 Hanfei Sun <hfsun.tju@gmail.com>
@@ -18,8 +18,8 @@ import sys
 import mf_corelib
 from check_file import check_bed,check_cmd,count_lines
 
-_fa_list={"hg19": ["chr%s.fa.masked"%_i for _i in range(1,23)+['X','Y']],
-          "mm9":["chr%s.fa.masked"%_i for _i in range(1,20)+['X','Y']]}
+_fa_list={"hg19": ["chr%s.fa.masked"%_i for _i in range(1,23)+['X','Y','M']],
+          "mm9":["chr%s.fa.masked"%_i for _i in range(1,20)+['X','Y','M']]}
 _chrom_len={"hg19":{'chr1': 249250621, 'chr2': 243199373, 'chr3': 198022430,
                     'chr4': 191154276, 'chr5': 180915260, 'chr6': 171115067,
                     'chr7': 159138663, 'chrX': 155270560, 'chr8': 146364022,
@@ -27,14 +27,16 @@ _chrom_len={"hg19":{'chr1': 249250621, 'chr2': 243199373, 'chr3': 198022430,
                     'chr12': 133851895, 'chr13': 115169878, 'chr14': 107349540,
                     'chr15': 102531392, 'chr16': 90354753, 'chr17': 81195210,
                     'chr18': 78077248, 'chr20':63025520, 'chrY': 59373566,
-                    'chr19': 59128983, 'chr22': 51304566, 'chr21': 48129895},
+                    'chr19': 59128983, 'chr22': 51304566, 'chr21': 48129895,
+                    'chrM':16571},
             "mm9":{'chr1': 197195432, 'chr2': 181748087, 'chr3': 159599783,
                    'chr4': 155630120, 'chr5': 152537259, 'chr6': 149517037,
                    'chr7': 152524553, 'chr8': 131738871, 'chr9': 124076172,
                    'chr10': 129993255, 'chr11': 121843856, 'chr12': 121257530,
                    'chr13': 120284312, 'chr14': 125194864, 'chr15': 103494974,
                    'chr16': 98319150, 'chr17': 95272651, 'chr18': 90772031,
-                   'chr19': 61342430, 'chrX': 166650296, 'chrY': 15902555,}}
+                   'chr19': 61342430, 'chrX': 166650296, 'chrY': 15902555,
+                   'chrM':16299}}
 # _fa_list and chrom_len are only for hg19
 _fasta_default={'hg19': "./hg19.fa",
                 'mm9': "./mm9.fa"}
